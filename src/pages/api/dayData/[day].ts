6b7df8d1b9246a29374d12be3789568dfc06722a
cpl121 <cpeon@boxfish.studio>
2024-12-02 13:34:53 +0100

    fix: error to fetch info

--- src/pages/api/dayData/[day].ts
+++ src/pages/api/dayData/[day].ts
@@ -2,7 +2,9 @@ import { getPuzzlesData, resolvePuzzle } from '@/lib'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     if (req.method === 'PATCH') {
         const { day } = req.query
         const { url } = req.body
