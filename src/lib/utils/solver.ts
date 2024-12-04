ea7609740d8e793ba99f3c0263ac314a7e132631
cpl121 <cpeon@boxfish.studio>
2024-12-04 07:42:05 +0100

    feat: day 3 part 1

--- src/lib/utils/solver.ts
+++ src/lib/utils/solver.ts
@@ -1,8 +1,9 @@
-import { puzzle1, puzzle2 } from '@/puzzles'
+import { puzzle1, puzzle2, puzzle3 } from '@/puzzles'
 
 const solvers = {
     1: puzzle1,
     2: puzzle2,
+    3: puzzle3,
 }
 
 export function solvePuzzle(id: number, input: string): string[] {
